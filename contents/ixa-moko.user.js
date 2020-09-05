412560e5aabeaa3dbccb53e7fe15d9b70cd11c26
KFC03724 <takayuki.oikawa@nifty.com>
2020-09-05 19:59:26 +0900

    2020.09.05 戦国IXA ツールスレPart45 697 までの改修点を反映済

--- contents/ixa-moko.user.js
+++ contents/ixa-moko.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IxaMoko
 // @description  戦国IXA用ツール コンテンツ
-// @version      10.20.202009.02
+// @version      10.20.202009.03
 // @author       nameless
 // @include      https://*.sengokuixa.jp/*
 // @exclude      https://sengokuixa.jp/*
@@ -20,7 +20,7 @@
 function MokoMain($) {
   console.debug('Load... MokoMain');
   "use strict";
-  var VERSION_NAME = "ver 10.20.202009.02";
+  var VERSION_NAME = "ver 10.20.202009.03";
 
 // === Plugin ===
 
